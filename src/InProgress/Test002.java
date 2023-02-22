e53f1af6cf2959d343cccddb677f69b851dc273c
GulcanAkuklu <gulcanakuklu3@gmail.com>
2023-02-22 19:44:41 +0100

    Added new lines in Test002

--- src/InProgress/Test002.java
+++ src/InProgress/Test002.java
@@ -6,5 +6,6 @@ public class Test002 {
     // work in progress
     public static void main(String[] args) {
         System.out.println("done");
+        System.out.println("Keeps History");
     }
 }
